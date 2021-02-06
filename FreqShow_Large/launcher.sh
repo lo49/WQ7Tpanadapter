7e00421040ebf4dd9f1c425bad1f4952bad7ae89
bouilo01 <loic.bouilly.external@somfy.com>
2021-02-06 20:13:02 +0100

    change launcher.sh

--- FreqShow_Large/launcher.sh
+++ FreqShow_Large/launcher.sh
@@ -3,6 +3,6 @@
 # navigate to  home directory, then to this directory, then execute python sript, then back home
 
 cd /
-cd home/pi/FreqShow_Large
+cd home/pi/WQ7Tpanadapter/FreqShow_Large
 sudo python freqshow.py
 cd /
